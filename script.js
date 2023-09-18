78c3801600d75aee8232765160fbd885b2beb1eb
Saleh Al-Saleh <saleh.alsaleh802@gmail.com>
2023-09-18 15:16:30 +0300

    add  all features for calculator
    add 2 decimal point percition for calculation and the ability to
    do multiple calculation without needing to press (=) for each one
    and connected buttons to funcitons on index.html file

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function devide(a, b){
     return a/b;
 }
 
-let A, B, operator;
+let A = 0, B = 0, operator = null;
+const stages = {
+    a: 1,
+    b: 2,
+    opert: 3
+}
+let currentVar = stages.a;
 
 function operatorfunc(operator, a, b){
 switch(operator){
@@ -31,6 +37,66 @@ switch(operator){
     default:
         return "no such operator";
 }
+
+}
+let screen = document.querySelector('.screen');
+
+function saveUserInputValue(input){
+   console.log(isNaN(input));
+   if(!isNaN(input) && currentVar == stages.a) {
+    A=  parseFloat((A + "")+  input);
+    currentVar = stages.b
+    currentVar=stages.a;
+    screen.textContent = A;
+   }else if((input == "+"|| input == "-" || input == "/"|| input == "*") && (currentVar == stages.a || currentVar == stages.opert)){
+    operator = input;
+    currentVar= stages.opert
+    screen.textContent = `${A} ${operator}`  ;
+   }else if( !isNaN(input) && (currentVar == stages.opert || currentVar == stages.b)){
+
+    B=  parseFloat((B + "")+  input);
+    currentVar = stages.b
+    screen.textContent =`${A} ${operator} ${B}` ;
+   }else if( input == "="){
+    if(operator == null){
+        return screen.textContent = "error 'must choose operator'"
+    }
+    A = Math.round(operatorfunc(operator, A, B) *100)/100;
+    screen.textContent = A;
+    B= 0;
+    operator=null;
+    currentVar =stages.a;
+   }else if(input == 'clear'){
+    A=0;
+    B=0;
+    operator=null;
+    currentVar= stages.a
+    screen.textContent = "0";
+   }else if((input == "+"|| input == "-" || input == "/"|| input == "*") && stages.b){
+    A = Math.round( operatorfunc(operator , A, B)*100)/100;
+    B=0;
+    operator=input;
+    currentVar=stages.opert;
+    screen.textContent = `${A} ${operator}`
+   }else if(input = 'delete'){
+    switch(currentVar){
+        case stages.a:
+            A= Math.floor( A /10) ;
+            screen.textContent = A;
+            break;
+        case stages.b:
+            B = Math.floor(A/10);
+            screen.textContent =`${A} ${operator} ${B}` ;
+            break;
+        case stages.opert:
+            screen.textContent = `${A} `  ;
+            operator = null;
+    }
+    
+
+   } else{
+    console.log("no such thing")
+   }
 
 }
 
